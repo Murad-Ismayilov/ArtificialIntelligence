6f3cad81248cfa73fe07a8882f1212a901e242ce
Murad İsmayilov <mismayilov429@gmail.com>
2024-07-18 15:12:20 +0400

    18 İyul

--- src/components/Intelligence.jsx
+++ src/components/Intelligence.jsx
@@ -37,6 +37,20 @@ const Intelligence = () => {
                 delay: 2500,
                 disableOnInteraction: false,
               }}
+              breakpoints={{
+                320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                },
+                768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                },
+                1024: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+                },
+            }}
             modules={[Autoplay]}
             className="mySwiper"
             >
